197ee5374fde98f7d297b91d97fbefab1884e0cd
akash-kamble63 <akamble2631@gmail.com>
2025-08-06 12:59:30 +0530

    fileds changes

---
+++ project-service/src/main/java/com/project_service/config/FeignClientConfig.java
@@ -0,0 +1,22 @@
+package com.project_service.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class FeignClientConfig implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate template) {
+		// TODO Auto-generated method stub
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getCredentials() instanceof String) {
+            String jwtToken = (String) authentication.getCredentials();
+            template.header("Authorization", "Bearer " + jwtToken);
+        }
+	}
+
+}
--- project-service/src/main/java/com/project_service/feign/UserClient.java
+++ project-service/src/main/java/com/project_service/feign/UserClient.java
@@ -3,12 +3,14 @@ package com.project_service.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.project_service.config.FeignClientConfig;
 import com.project_service.dto.UserDTO;
 
 
-@FeignClient(name = "auth-service", url = "http://localhost:8081/api/users")
+@FeignClient(name = "auth-service", url = "http://localhost:8081/api", configuration = FeignClientConfig.class)
 public interface UserClient {
 	@GetMapping("/users/{id}")
-    UserDTO getUserById(@PathVariable("id") Long userId);
+    UserDTO getUserById(@PathVariable("id") Long userId, @RequestHeader("Authorization") String token);
 }
